120b8ec76ee9870e36dcccacf30434723fa7927e
nana <jooankrah@gmail.com>
2020-11-20 06:58:28 +0000

    modified:   controllers/product.js 	deleted:    files/image-1580664754571.jpg 	deleted:    files/image-1592439491428.jpg 	modified:   routers/product.js

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,41 @@
-const product = require('../models/product')
+const Product = require('../models/product')
 
 //create a product
-const newProduct = product.c
+const newProduct =async (req,res)=>{
+
+   await Product.create(productItem, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+        console.log(newlyCreated); 
+        }
+    });
+
+}
+
+//get all products
+const getProducts = async (req,res)=>{
+  await  Product.find({},(err,products)=>{
+        if (err){
+            res.sendStatus(404)
+        }
+      return  res.json(products)
+    })
+}
+
+
+
+//get a specific product
+const getProduct = async (req,res)=>{
+ const product_id = req.params.id
+ await Product.findById({id:product_id},(err,product)=>{
+        if (err) {
+            res.sendStatus(404)
+        } else {
+         return   res.json(product)
+        }
+ })
+
+}
+
+module.exports={newProduct,getProducts,getProduct}
