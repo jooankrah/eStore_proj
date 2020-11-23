a4d042364eba3b0c19a018d0700a7e3f83d92ba7
Jonathan Nsiah <jooankrah@gmail.com>
2020-11-23 13:48:22 +0000

    commit

--- public/js/custom/custom2.js
+++ public/js/custom/custom2.js
@@ -1,34 +1,41 @@
 function getCart() {
          
            const data = JSON.parse(localStorage.getItem('cart'))
-           for(var i=0;i<data.length;i++){
+            if (data==null) {
 
-           const cartItems =  `<tr id= ${data[i].productId}>
-                                    <td class="cart_product">
-                                        <img src="${data[i].image}" style="height: 100px; weight: 100px" alt="">
-                                    </td>
-                                    <td class="cart_description">
-                                        <h4><a href="">${data[i].name}</a></h4>
-                                    </td>
-                                    <td class="cart_price">
-                                        <p>${data[i].price}</p>
-                                    </td>
-                                    <td class="cart_quantity">
-                                        <div class="cart_quantity_button">
-                                            <input class="cart_quantity_input" type="text" name="quantity" value=${data[i].quantity} autocomplete="off" size="2">
-                                        </div>
-                                    </td>
-                                    <td class="cart_total">
-                                        <p class="cart_total_price">$59</p>
-                                    </td>
-                                    <td class="cart_delete">
-                                        <button data-id = ${data[i]} class="cart_quantity_delete removeItem" href=""><i class="fa fa-times"></i></button>
-                                    </td>
-                                </tr>`
+            const cartItems = `<tr><div class="text-center">No Items added to cart<div></tr>`
+            $('#cartbody').after(cartItems);
+           } else {
+              for(var i=0;i<data.length;i++){
+              
+            const cartItems =  `<tr id= ${data[i].productId}>
+              <td class="cart_product">
+                  <img src="${data[i].image}" style="height: 100px; weight: 100px" alt="">
+              </td>
+              <td class="cart_description">
+                  <h4><a href="">${data[i].name}</a></h4>
+              </td>
+              <td class="cart_price">
+                  <p>${data[i].price}</p>
+              </td>
+              <td class="cart_quantity">
+                  <div class="cart_quantity_button">
+                      <input class="cart_quantity_input" type="text" name="quantity" value=${data[i].quantity} autocomplete="off" size="2">
+                  </div>
+              </td>
+              <td class="cart_total">
+                  <p class="cart_total_price">$59</p>
+              </td>
+              <td class="cart_delete">
+                  <button data-id = ${data[i].productId} class="cart_quantity_delete removeItem" href=""><i class="fa fa-times"></i></button>
+              </td>
+          </tr>`
+          $('#cartbody').after(cartItems);
 
-               $('#cartbody').after(cartItems);
+        }
 
            }
+           
 
        }
 
@@ -76,7 +83,7 @@ $(document).ready(function () {
     console.log(toBeremoved)
 
     var cart = data.filter(function(value, index, arr){ 
-        return value != toBeremoved;
+        return value.productId != toBeremoved;
     });
     localStorage.setItem('cart',JSON.stringify(cart))
 
